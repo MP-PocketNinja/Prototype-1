ad379649746fd817562011c10a0e96a5b02c698d
Kaiser, Brett <56174779+MP-PocketNinja@users.noreply.github.com>
2019-11-06 13:49:05 -0800

    Steps 1 through 7
    fixed camera, and actually thats pretty much it

--- Assets/Challenge 1/Scripts/SpinPropeller.cs
+++ Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayerX : MonoBehaviour
+public class SpinPropeller : MonoBehaviour
 {
     public GameObject Player;
-    private Vector3 offset = new Vector3(30, 0, 10);
-    private Vector3 offset1 = new Vector3(0, -90, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Player.transform.position + offset;
+     
     }
 }
